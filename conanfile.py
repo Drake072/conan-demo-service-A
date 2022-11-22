56fcd7caf018cb6bd9464794781383ad08a84501
Da Ke <da_ke@outlook.com>
2022-11-23 03:43:17 +0800

    Re-structure project

--- conanfile.py
+++ conanfile.py
@@ -19,9 +19,9 @@ class LoggerConan(ConanFile):
     default_options = {"shared": False, "fPIC": True}
 
     # Sources are located in the same place as this recipe, copy them to the recipe
-    # exports_sources = "CMakeLists.txt", "src/*", "include/*"
+    exports_sources = "src/CMakeLists.txt", "src/*.cpp", "src/include/*"
 
-    # generators = ["cmake", "cmake_find_package"]
+    generators = "cmake_find_package"
 
     def requirements(self):
         self.requires("logger/1.0")
@@ -31,19 +31,30 @@ class LoggerConan(ConanFile):
             del self.options.fPIC
 
     def layout(self):
-        cmake_layout(self)
+        cmake_layout(self, src_folder="src")
+        # self.folders.source = "src"
 
     def generate(self):
         tc = CMakeToolchain(self)
         tc.generate()
 
     def build(self):
+        # self.run('cd src && cmake . %s' % cmake.command_line)
+        # self.run("cd src && cmake --build . %s" % cmake.build_config)
         cmake = CMake(self)
+        # print("install_folder -----------------------" + self.install_folder)
+        # print("build_folder -----------------------" + self.build_folder)
+        # print("package_folder-----------------------" + self.package_folder)
+        # print("generators_folder-----------------------" + self.generators_folder)
+        print("CMAKE_MODULE_PATH:-----------------------" + self.build_folder)
+        cmake.configure({'CMAKE_MODULE_PATH': self.build_folder})
+        # cmake.configure({'CMAKE_MODULE_PATH': self.folders.generators_folder})
         cmake.configure()
         cmake.build()
 
     def package(self):
-        self.copy("*")
+        self.copy("*.a", dst="lib")
+        self.copy("*.h")
 
     def package_info(self):
         # self.cpp_info.libs = ["serviceA"]
